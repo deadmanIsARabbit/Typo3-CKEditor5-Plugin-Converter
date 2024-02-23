4d379d7b770cc782cd812780edeac57e4f399c31
Matthaeus Kuhn <m.kuhn@ayeq-benu.com>
2024-02-23 09:19:08 +0100

    added minor changes to satisfy some npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -14,17 +14,22 @@ module.exports = {
             'ckeditor5/src/core': '/src/cke-translator-core.js',
             'ckeditor5/src/ui': '/src/cke-translator-ui.js',
             'ckeditor5/src/utils': '/src/cke-translator-utils.js'
+        },
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "buffer": require.resolve("buffer")
         }
     },
     externals: {
         '@typo3/ckeditor5-bundle.js': '@typo3/ckeditor5-bundle.js'
     },
-    entry: './node_modules/@ckeditor/ckeditor5-font/src/font.js',
+    //entry: './node_modules/@ckeditor/ckeditor5-font/src/font.js',
     // Another example: enty to convert the code-block-plugin
-    // entry: './node_modules/@ckeditor/ckeditor5-code-block/src/codeblock.js',
+    entry: './node_modules/ckeditor5-heading-with-id/src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'cke-font-plugin.js',
+        filename: 'cke-hwi.js',
         library: {
             type: 'module',
         },
